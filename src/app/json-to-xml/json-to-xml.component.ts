a1f0b7330da6d33a764a42e45ae825bf47abeeae
Stevan Radovanovic <stevan.radovanovic@mmklab.org>
2020-10-13 12:58:35 +0200

    [Commit #5] Updated JSON to XML

--- src/app/json-to-xml/json-to-xml.component.ts
+++ src/app/json-to-xml/json-to-xml.component.ts
@@ -11,9 +11,48 @@ export class JsonToXmlComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  OBJtoXML(obj) {
+    var xml = '';
+    for (var prop in obj) {
+      xml += obj[prop] instanceof Array ? '' : '<' + prop + '>';
+      if (obj[prop] instanceof Array) {
+        for (var array in obj[prop]) {
+          xml += '<' + prop + '>';
+          xml += this.OBJtoXML(new Object(obj[prop][array]));
+          xml += '</' + prop + '>';
+        }
+      } else if (typeof obj[prop] == 'object') {
+        xml += this.OBJtoXML(new Object(obj[prop]));
+      } else {
+        xml += obj[prop];
+      }
+      xml += obj[prop] instanceof Array ? '' : '</' + prop + '>';
+    }
+    var xml = xml.replace(/<\/?[0-9]{1,}>/g, '');
+    return xml;
+  }
+
   onClickConvertXml() {
-    const xml = toXML(this.mockData.items);
-    console.dirxml(xml);
+    let xml = '';
+    xml += '<blinking-xml>';
+    xml += this.OBJtoXML(this.mockData.items);
+    xml += '</blinking-xml>';
+
+    let blob = new Blob([xml], { type: 'text/xml;charset=utf-8;' });
+    if (navigator.msSaveBlob) {
+      navigator.msSaveBlob(blob, 'Blinking');
+    } else {
+      let link = document.createElement('a');
+      if (link.download !== undefined) {
+        const url = URL.createObjectURL(blob);
+        link.setAttribute('href', url);
+        link.setAttribute('download', 'Blinking');
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    }
   }
 
   mockData = {
